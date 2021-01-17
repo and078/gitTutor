cc99c65455deb34e692284a30fe7d23da4b56897
and078 <andreynovikov078@gmail.com>
2021-01-17 14:51:59 +0200

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(1);
+//fibonacci:
+function fibonacci(number) {
+	if(number == 0){
+		return 0;
+	}
+	if(number == 1){
+		return 1;
+	}
+	else
+		return fibonacci(number - 2) + fibonacci(number - 1);
+}
+
+for(let i = 0; i < 51; i++){
+	console.log(i + " - " + fibonacci(i));	
+}
+
+
